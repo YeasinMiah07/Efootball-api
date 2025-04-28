faaeed51a2b7d8b79999120148e7ec5006c1ede4
YeasinMiah07 <120682243+YeasinMiah07@users.noreply.github.com>
2025-04-28 14:57:31 +0600

    Add files via upload

---
+++ players.js
@@ -0,0 +1,44 @@
+export const players = [
+    {
+        "name": "Lionel Messi",
+        "image": "https://cdn.sofifa.net/players/158/023/24_60.png",
+        "position": "RW",
+        "playingStyle": "Creative Playmaker",
+        "cost": "100",
+        "height": "170",
+        "weight": "72",
+        "age": "36",
+        "strongerFoot": "Left",
+        "stats": {
+            "offensiveAwareness": "94",
+            "dribbling": "96",
+            "finishing": "92",
+            "speed": "80",
+            "acceleration": "85",
+            "stamina": "70",
+            "defensiveAwareness": "35",
+            "tackling": "45"
+        }
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "image": "https://cdn.sofifa.net/players/020/801/24_60.png",
+        "position": "ST",
+        "playingStyle": "Goal Poacher",
+        "cost": "95",
+        "height": "187",
+        "weight": "83",
+        "age": "39",
+        "strongerFoot": "Right",
+        "stats": {
+            "offensiveAwareness": "92",
+            "dribbling": "85",
+            "finishing": "95",
+            "speed": "82",
+            "acceleration": "77",
+            "stamina": "75",
+            "defensiveAwareness": "30",
+            "tackling": "40"
+        }
+    }
+];
